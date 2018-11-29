fdd3b55d24af14f6595647d75bddaf0d80d4c68d
briannoogin <brian.qh.nguyen@gmail.com>
2018-11-29 14:57:18 -0600

    rename vars

--- icnn.py
+++ icnn.py
@@ -21,7 +21,7 @@ print('tf seed: ', tf_rand)
 #icnn_arch = [256, 128, 128, 64, 32, 32]
 icnn_arch = [2000, 2000]
 print_weights = False
-clipped_weights = {}
+weights = {}
 normalize_data = False
 
 # number of variables we consider in each input
@@ -33,14 +33,14 @@ batch_size = 32
 
 init = tf.initialize_all_variables()
 
-def clipped_fc(x, input_size, output_size, layer_num, softmax=True):
+def fc(x, input_size, output_size, layer_num, softmax=True):
     var_name = 'W_' + str(layer_num)
     W = tf.Variable(tf.truncated_normal([input_size, output_size]), name=var_name)
 
     bias_name = 'B_' + str(layer_num)
     b = tf.Variable(tf.truncated_normal([output_size]), name=bias_name)
 
-    clipped_weights[var_name] = W
+    weights[var_name] = W
 
     if softmax:
         return tf.nn.relu(tf.matmul(x, W) + b)
@@ -49,20 +49,20 @@ def clipped_fc(x, input_size, output_size, layer_num, softmax=True):
 
 def fcnn(input_data, arch):
     # first layer
-    nn = clipped_fc(x=input_data, 
+    nn = fc(x=input_data, 
                     input_size=num_vars, 
                     output_size=arch[0], 
                     layer_num=1)
 
     # hidden layers
     for layer in range(1, len(arch)):
-        nn = clipped_fc(x=nn, 
+        nn = fc(x=nn, 
                         input_size=arch[layer-1], 
                         output_size=arch[layer], 
                         layer_num=layer + 1)
 
     # output layer, don't use softmax
-    nn = clipped_fc(x=nn, 
+    nn = fc(x=nn, 
                     input_size=arch[len(arch) - 1], 
                     output_size=1, 
                     layer_num='out', 
@@ -79,9 +79,6 @@ Y_ = tf.placeholder(tf.float32, [None, 1])
 
 # entire model
 icnn_out = fcnn(X, icnn_arch)
-
-# create clipping ops for each weight variable in our icnn
-clip_ops = [tf.assign(clipped_weights[w], tf.maximum(0., clipped_weights[w])) for w in clipped_weights]
 
 cost = tf.reduce_mean(tf.square(icnn_out - Y_))
 train = tf.train.AdamOptimizer(lr).minimize(cost)
@@ -111,10 +108,6 @@ with tf.Session() as sess:
 
         # train
         _, current_cost = sess.run([train, cost], feed_dict={X: shuffled_x, Y_: shuffled_y})
-
-        # apply weight clipping
-        for op in clip_ops:
-            sess.run(op)
 
         # print training progress
         if i % 100 == 0:
@@ -159,28 +152,3 @@ with tf.Session() as sess:
     print('Architecture:',icnn_arch)
     print('lr_:',lr)
     print('iterations:',iter_)
-
-'''
-## legacy icnn construction method
-
-# fc1
-W_1 = tf.Variable(tf.truncated_normal([num_vars, fc1_size]))
-b_1 = tf.Variable(tf.truncated_normal([fc1_size]))
-fc1 = tf.nn.softmax(tf.matmul(X, W_1) + b_1)
-
-# fc2 
-W_2 = tf.Variable(tf.truncated_normal([fc1_size,fc2_size]))
-b_2 = tf.Variable(tf.truncated_normal([fc2_size]))
-fc2 = tf.nn.softmax(tf.matmul(fc1, W_2) + b_2)
-
-# fc3
-W_3 = tf.Variable(tf.truncated_normal([fc2_size,1]))
-b_3 = tf.Variable(tf.truncated_normal([1]))
-out = tf.matmul(fc2, W_3) + b_3
-
-# clipping op so weights stay in [0, inf)
-clip_W_1 = W_1.assign(tf.maximum(0., W_1))
-clip_W_2 = W_2.assign(tf.maximum(0., W_2))
-clip_W_3 = W_3.assign(tf.maximum(0., W_3))
-clip = tf.group(clip_W_1, clip_W_2, clip_W_3)
-'''
