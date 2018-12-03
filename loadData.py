3adfabfcf5617fb786091e9405185d719c964d9c
briannoogin <brian.qh.nguyen@gmail.com>
2018-12-03 16:47:43 -0600

    fixed load data

--- loadData.py
+++ loadData.py
@@ -2,33 +2,26 @@ import numpy as np
 import os
 import pandas as pd
 
-def load_data(path, omit=False):
-    f = pd.read_table(path, header=None, delim_whitespace=True)
-    train, test = [], []
-    
-    num_data = len(f)
-    for x in range(0, num_data):
-        # skip data that has -200
-        skip_flag = False
-        
-        # all pieces of data are used besides str name, and origin (last 2 indices)
-        row_of_data = [f.loc[x][i] for i in range(1,15)]
-        
-        for j in row_of_data:
-            if float(j) == -200:
-                skip_flag = True 
-        
-        # if any piece of data is unknown, we don't add it to our dataset
-        if skip_flag and omit:
-            continue
-        
-        # append y_ to test, and x to train
-        test.append(f.loc[x][0])
-        train.append(row_of_data)
-
-    return train, test
+def load_data(path):
+    file = open(path,'r')
+    data = []
+    for line in file:
+        data.append(line.split())
+    file.close()
+    labels = []
+    # take away the headers
+    data = data[1:len(data)] 
+    # convert from string to float
+    data = [[float(dataPoint) for dataPoint in row] for row in data]
+     # extract the classes and remove the classes column
+    for index in range(len(data)):
+        labels.append(data[index][-1])
+        del data[index][-1]
+    return [data,labels]
 def deleteZeros(path):
      f = pd.read_table(path, header=None, delim_whitespace=True)
      f= f[f[23] != 0]
+     np.savetxt(r'mHealth.log', f.values, fmt='%d')
+     print(f[23].value_counts())
      return f
-deleteZeros('mHealth_subject1.log')
+load_data('mHealth.log')
