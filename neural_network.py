c53551952a2ccc9c49a869c2ee863ed058b6c8a2
briannoogin <brian.qh.nguyen@gmail.com>
2019-01-05 15:59:05 -0600

    started working on active guard by implemnting lambda layers for dropout

--- neural_network.py
+++ neural_network.py
@@ -17,6 +17,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense,Input,add,multiply,Lambda
 from keras import regularizers
+from keras import optimizers
 from keras.utils import plot_model
 from keras.optimizers import SGD
 from keras.models import Model
@@ -24,10 +25,11 @@ from keras.backend import ones
 from keras.backend import zeros
 from keras.backend import eval
 from keras.backend import constant
+import keras.backend as K
 from ann_visualizer.visualize import ann_viz;
 
 import tensorflow as tf
-
+from NeuralNumber import NeuralNumber
 import time
 # assumes that the hidden units and the regularization constant are consistent throughout the network
 # returns the baseline_model 
@@ -78,6 +80,7 @@ def define_baseline_functional_model(num_vars,num_classes,hidden_units, regulari
 def add_node_layers(input_tensors):
     first_input = input_tensors[0]
     second_input = input_tensors[1]
+    # TODO: check for NaNs, used for failure cases
     return add([first_input,second_input])
 
 # lambda function to add the beginning physical node in the network
@@ -86,12 +89,38 @@ def add_first_node_layers(input_tensor):
     first_input = input_tensor
     return first_input
 
-# returns fixed guard model with 10 hidden layers
+# lambda function based on the probability will randonmly drop a node
+# input: layer tensor and probability 
+# output: outputs either a tensor of 0s with boolean indicating failure or the orginal tensor with a boolean indicating survival
+def dropout_layer(input_tensor):
+    layer = input_tensor[0]
+    survive_prob = input_tensor[1]
+    num = np.random.random()
+    print('hi')
+    if(num > survive_prob):
+        # drop out node by setting output to 0
+        print(layer, "was dropped out")
+        return [constant(0,layer.shape()),True]
+    else:
+        return [layer,False]
+# lambda function that does smart guessing based on training data when there is no data flow
+# input: probabilties for each class
+def smart_guessing(input_tensor):
+    num = np.random.random()
+
+# function that returns whether there is data flow in the network
+def data_flow(failure_list):
+    count = 0;
+    for failure in failure_list:
+        if failure:
+            count+=1
+    return count >= 2
+# returns active guard model with 10 hidden layers
 # f1 = fog node 2 = 1st hidden layer
 # f2 = fog node 2 = 2nd and 3rd hidden layer
 # f3 = fog node 3 = 4th-6th hidden layers
 # c = cloud node = 7th-10th hidden layer and output layer 
-def define_model_with_connections(num_vars,num_classes,hidden_units,regularization,survive_rates):
+def define_active_guard_model_with_connections(num_vars,num_classes,hidden_units,regularization,survive_rates):
     # calculate connection weights
     # naming convention:
     # ex: f1f2 = connection between fog node 1 and fog node 2
@@ -109,42 +138,118 @@ def define_model_with_connections(num_vars,num_classes,hidden_units,regularizati
     multiply_weight_layer_f2c = Lambda((lambda x: x * connection_weight_f2c), name = "connection_weight_f2c")
     multiply_weight_layer_f3c = Lambda((lambda x: x * connection_weight_f3c), name = "connection_weight_f3c")
 
+    # define probabilties for failure
+    dropout_fog_node_1 = 1 - survive_rates[0]
+    dropout_fog_node_2 = 1 - survive_rates[1]
+    dropout_fog_node_3 = 1 - survive_rates[2]
+
     # one input layer
     input_layer = Input(shape = (num_vars,))
 
     # 10 hidden layers, 3 fog nodes
     # first fog node
-    f1 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l2(regularization),name="fog1_output_layer")(input_layer)
+    f1 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="fog1_output_layer")(input_layer)
+    f1,f1_failed = Lambda(dropout_layer,name="f1_dropout",training=True)([f1,dropout_fog_node_1])
     f1f2 = multiply_weight_layer_f1f2(f1)
     connection_f2 = Lambda(add_first_node_layers,name="F1_F2")(f1f2)
+    #TODO: add branch lambda if there are multiple failures?
 
     # second fog node
-    f2 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l2(regularization),name = "fog2_input_layer")(connection_f2)
-    f2 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l2(regularization),name="fog2_output_layer")(f2)
+    f2 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="fog2_input_layer")(connection_f2)
+    f2 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="fog2_output_layer")(f2)
+    f2,f2_failed = Lambda(dropout_layer,name="f2_dropout",training=True)([f2,dropout_fog_node_2])
     f1f3 = multiply_weight_layer_f1f3(f1)
     f2f3 = multiply_weight_layer_f2f3(f2)
     connection_f3 = Lambda(add_node_layers,name="F1F2_F3")([f1f3,f2f3])
 
     # third fog node
-    f3 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l2(regularization),name="fog3_input_layer")(connection_f3)
-    f3 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l2(regularization),name="fog3_layer_1")(f3)
-    f3 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l2(regularization),name="fog3_output_layer")(f3)
+    f3 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="fog3_input_layer")(connection_f3)
+    f3 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="fog3_layer_1")(f3)
+    f3 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="fog3_output_layer")(f3)
+    f3,f3_failed = Lambda(dropout_layer,name="f3_dropout",training=True)([f3,dropout_fog_node_3])
     f2c = multiply_weight_layer_f2c(f2)
     f3c = multiply_weight_layer_f3c(f3)
     connection_cloud = Lambda(add_node_layers,name="F2F3_FC")([f2c,f3c])
 
     # cloud node
-    cloud = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l2(regularization),name="cloud_input_layer")(connection_cloud)
-    cloud = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l2(regularization),name="cloud_layer_1")(cloud)
-    cloud = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l2(regularization),name="cloud_layer_2")(cloud)
-    cloud = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l2(regularization),name="cloud_layer_3")(cloud)
+    cloud = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="cloud_input_layer")(connection_cloud)
+    cloud = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="cloud_layer_1")(cloud)
+    cloud = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="cloud_layer_2")(cloud)
+    cloud = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="cloud_layer_3")(cloud)
 
     # one output layer
-    output_layer = Dense(units=num_classes,activation='softmax',name = "output")(cloud)
-    model = Model(inputs=input_layer, outputs=output_layer)
+    has_data_flow = data_flow([f1_failed,f2_failed,f3_failed])
+    normal_output_layer = Dense(units=num_classes,activation='softmax',name = "output")(cloud)
+    #smart_guessing_layer = Lambda(smart_guessing)
+    #output_layer = K.switch(data_flow,normal_output_layer,smart_guessing_layer)
+    # TODO: make a lambda functoin that checks if there is no data connection flow and does smart random guessing
+    model = Model(inputs=input_layer, outputs=normal_output_layer)
+    # sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    # adagrad = optimizers.Adagrad(lr=0.01, epsilon=None, decay=0.0)
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
+# returns fixed guard model with 10 hidden layers
+# f1 = fog node 2 = 1st hidden layer
+# f2 = fog node 2 = 2nd and 3rd hidden layer
+# f3 = fog node 3 = 4th-6th hidden layers
+# c = cloud node = 7th-10th hidden layer and output layer 
+def define_model_with_connections(num_vars,num_classes,hidden_units,regularization,survive_rates):
+    # calculate connection weights
+    # naming convention:
+    # ex: f1f2 = connection between fog node 1 and fog node 2
+    # ex: f2c = connection between fog node 2 and cloud node
+    connection_weight_f1f2 = 1
+    connection_weight_f1f3 =  (survive_rates[0] + survive_rates[1]) / survive_rates[0] 
+    connection_weight_f2f3 = (survive_rates[0] + survive_rates[1]) / survive_rates[1]
+    connection_weight_f2c = (survive_rates[1] + survive_rates[2]) / survive_rates[1] 
+    connection_weight_f3c = (survive_rates[1] + survive_rates[2]) / survive_rates[2]
+
+    # define lambdas for multiplying node weights by connection weight
+    multiply_weight_layer_f1f2 = Lambda((lambda x: x * connection_weight_f1f2), name = "connection_weight_f1f2")
+    multiply_weight_layer_f1f3 = Lambda((lambda x: x * connection_weight_f1f3), name = "connection_weight_f1f3")
+    multiply_weight_layer_f2f3 = Lambda((lambda x: x * connection_weight_f2f3), name = "connection_weight_f2f3")
+    multiply_weight_layer_f2c = Lambda((lambda x: x * connection_weight_f2c), name = "connection_weight_f2c")
+    multiply_weight_layer_f3c = Lambda((lambda x: x * connection_weight_f3c), name = "connection_weight_f3c")
+
+    # one input layer
+    input_layer = Input(shape = (num_vars,))
+
+    # 10 hidden layers, 3 fog nodes
+    # first fog node
+    f1 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="fog1_output_layer")(input_layer)
+    f1f2 = multiply_weight_layer_f1f2(f1)
+    connection_f2 = Lambda(add_first_node_layers,name="F1_F2")(f1f2)
+
+    # second fog node
+    f2 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="fog2_input_layer")(connection_f2)
+    f2 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="fog2_output_layer")(f2)
+    f1f3 = multiply_weight_layer_f1f3(f1)
+    f2f3 = multiply_weight_layer_f2f3(f2)
+    connection_f3 = Lambda(add_node_layers,name="F1F2_F3")([f1f3,f2f3])
+
+    # third fog node
+    f3 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="fog3_input_layer")(connection_f3)
+    f3 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="fog3_layer_1")(f3)
+    f3 = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="fog3_output_layer")(f3)
+    f2c = multiply_weight_layer_f2c(f2)
+    f3c = multiply_weight_layer_f3c(f3)
+    connection_cloud = Lambda(add_node_layers,name="F2F3_FC")([f2c,f3c])
+
+    # cloud node
+    cloud = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="cloud_input_layer")(connection_cloud)
+    cloud = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="cloud_layer_1")(cloud)
+    cloud = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="cloud_layer_2")(cloud)
+    cloud = Dense(units=hidden_units,activation='relu',kernel_regularizer=regularizers.l1(regularization),name="cloud_layer_3")(cloud)
+
+    # one output layer
+    output_layer = Dense(units=num_classes,activation='softmax',name = "output")(cloud)
+    # TODO: make a lambda functoin that checks if there is no data connection flow and does smart random guessing
+    model = Model(inputs=input_layer, outputs=output_layer)
+    # sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    # adagrad = optimizers.Adagrad(lr=0.01, epsilon=None, decay=0.0)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 # fails node by making the physical node return 0
 # node_array: bit array, 1 corresponds to alive, 0 corresponds to failure
 def fail_node(model,node_array):
@@ -158,9 +263,13 @@ def fail_node(model,node_array):
             layer_weights = layer.get_weights()
             # make new weights for the connections
             new_weights = np.zeros(layer_weights[0].shape)
+            #new_weights[:] = np.nan # set weights to nan
             # make new weights for biases
             new_bias_weights = np.zeros(layer_weights[1].shape)
+            #new_bias_weights[:] = np.nan # set weights to nan
             layer.set_weights([new_weights,new_bias_weights])
+            print(layer_name, "was failed")
+        #print(model.get_layer(name = "fog3_output_layer").get_weights())
 
 # save weights of network 
 def save_weights(model):
@@ -173,8 +282,12 @@ def save_weights(model):
 def load_model(input_size, output_size, hidden_units, regularization, weights_path,model_type):
     if model_type == 0:
         model = define_baseline_functional_model(input_size,output_size,hidden_units,regularization)
-    if model_type == 1:
+    elif model_type == 1:
         model = define_model_with_connections(input_size,output_size,hidden_units,regularization,[.99,.96,.92])
+    elif model_type == 2:
+        model = define_active_guard_model_with_connections(input_size,output_size,hidden_units,regularization,[.99,.96,.92])
+    else:
+        raise ValueError("Incorrect model type")
     model.load_weights(weights_path)
     #print_weights(model)
     return model
@@ -194,22 +307,26 @@ def print_weights(model):
         print(layer.get_config(),layer.get_weights())
 
 # trains and returns the model 
-def train_model(training_data,training_labels,validation_data,validation_labels,model_number):
+def train_model(training_data,training_labels,validation_data,validation_labels,model_type):
     # variable to save the model
-    save_model = True
+    save_model = False
 
-    # train 5 models on the same training data and choose the model with the highest validation accuracy 
+    # train 1 model on the same training data and choose the model with the highest validation accuracy 
     max_acc = -1
     best_model = None
     num_iterations = 1
     for model_iteration in range(0,num_iterations):   
         # create model
-        if model_number == 0:
-            model = define_baseline_functional_model(num_vars,num_classes,50,.01)
-        if model_number == 1:
-            model = define_model_with_connections(num_vars,num_classes,50,.01,[.99,.96,.92])
+        if model_type == 0:
+            model = define_baseline_functional_model(num_vars,num_classes,50,.001)
+        elif model_type == 1:
+            model = define_model_with_connections(num_vars,num_classes,10,0,[.99,.96,.92])
+        elif model_type == 2:
+            model = define_active_guard_model_with_connections(num_vars,num_classes,10,0,[.99,.96,.92])
+        else:
+            raise ValueError("Incorrect model type")
         # fit model on training data
-        model.fit(training_data,training_labels, epochs=100, batch_size=128,verbose=1,shuffle = True)
+        model.fit(training_data,training_labels, epochs=25, batch_size=128,verbose=1,shuffle = True)
         # test model on validation data
         error,accuracy = model.evaluate(validation_data,validation_labels,batch_size=128,verbose=0)
         if(accuracy > max_acc):
@@ -230,23 +347,37 @@ def train_model(training_data,training_labels,validation_data,validation_labels,
     print("Precision on validation set:",val_precision)
     print("Recall on validation set:",val_recall, '\n')
     if save_model:
-        best_model.save_weights('10 layers 100 units .01 reg adam hyperconnections model_weights.h5')
+        best_model.save_weights('inverted connection weights.h5')
     return model
 
 # returns the test performance measures 
-def test_model(model,test_data,test_labels):
+def test_model(model,test_data,test_labels,set_name):
     test_model_preds = predict_classes(model,test_data)
     test_precision = precision_score(test_labels,test_model_preds,average='macro')
     test_recall = precision_score(test_labels,test_model_preds,average='macro')
     test_error,test_accuracy = model.evaluate(test_data,test_labels,batch_size=128,verbose=0)
-    print("Accuracy on test set:", test_accuracy)
-    print("Precision on test set:",test_precision)
-    print("Recall on test set:",test_recall)
+    if set_name == 'validation':
+        print("Accuracy on validation set:", test_accuracy)
+        print("Precision on validation set:",test_precision)
+        print("Recall on validation set:",test_recall)
+    if set_name == 'test':
+        print("Accuracy on test set:", test_accuracy)
+        print("Precision on test set:",test_precision)
+        print("Recall on test set:",test_recall)
 
 # returns the classes prediction from a Keras functional model
 def predict_classes(functional_model,data):
     y_prob = functional_model.predict(data) 
     return y_prob.argmax(axis=-1)
+
+# prints the output of a layer of the model
+def print_layer_output(model,data):
+        layer_output = model.get_layer(name = "fog2_output_layer").output
+        output_model = Model(inputs = model.input,outputs=layer_output)
+        intermediate_output = output_model.predict(data)
+        print(layer_output.shape)
+        print(intermediate_output.shape)
+        print(intermediate_output)
 
 if __name__ == "__main__":
     # load data
@@ -258,11 +389,12 @@ if __name__ == "__main__":
     num_vars = len(training_data[0])
     num_classes = 13
 
-    load_weights = True
+    load_weights = False
     if load_weights:
-        path = '10 layers 50 units .01 reg adam hyperconnections model_weights.h5'
-        model = load_model(input_size = num_vars, output_size = num_classes, hidden_units = 50, regularization = .01, weights_path = path, model_type = 1)
-        fail_node(model,[1,1,0])
+        path = 'inverted connection weights.h5'
+        model = load_model(input_size = num_vars, output_size = num_classes, hidden_units = 10, regularization = 0, weights_path = path, model_type = 2)
+        fail_node(model,[1,1,1])
+        #print_layer_output(model,validation_data)
         #plot_model(model,to_file = "model_with_connections.png",show_shapes = True)
         #ann_viz(model, title="Artificial Neural network - Model Visualization")
     else:
@@ -270,6 +402,6 @@ if __name__ == "__main__":
         model = train_model(training_data,training_labels,validation_data,validation_labels,1)
         end = time.time()
         print("Time elapsed:", end-start)
-    test_model(model,validation_data,validation_labels)
-    test_model(model,test_data,test_labels)
+    test_model(model,validation_data,validation_labels,'validation')
+    test_model(model,test_data,test_labels,'test')
   
