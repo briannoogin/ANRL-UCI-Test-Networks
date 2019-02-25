7657381953e565bb9cdef9281eb8c3f78fe01a99
Alan Liao <alanliao7@yahoo.com>
2019-02-25 00:23:41 -0600

    combined batches into one file

--- parse_gaslogs.py
+++ parse_gaslogs.py
@@ -1,26 +1,35 @@
 import numpy as np
 import pandas as pd
 import sys
+import os
+import shutil
 
-#ARGS:
-#1: INPUT FILE
-#2: FORMATTED OUTPUT FILE
+# counts all classes
+def countClasses(path):
+     f = pd.read_table(path, header=None, delim_whitespace=True)
+     # np.savetxt(path, f.values, fmt='%s')
+     #dataframe.sort_index(by='count', ascending=[True])
+     print(f[0].value_counts().sort_index(ascending=[True]).tolist())
+     return f
 
-for i in range(1,11):
-    with open("parsed_gas/batch" + str(i) + ".dat", mode="w") as out:
-        with open("Gas_sensor_dataset/dataset/batch" + str(i) + ".dat") as log:
-            for line in log:
-                log_samples = line.split()
-                out.write("".join(num.ljust(18) for num in log_samples))
-                out.write("\n");
+def parseFilesAndCount():
+    for i in range(1,11):
+        with open("parsed_gas/batch" + str(i) + ".dat", mode="w") as out:
+            with open("Gas_sensor_dataset/dataset/batch" + str(i) + ".dat") as log:
+                for line in log:
+                    log_samples = line.split()
+                    out.write("".join(num.ljust(18) for num in log_samples))
+                    out.write("\n");
 
-    # deletes all examples with 0 as label
-    def countClasses(path):
-         f = pd.read_table(path, header=None, delim_whitespace=True)
-         # np.savetxt(path, f.values, fmt='%s')
-         #dataframe.sort_index(by='count', ascending=[True])
-         print(f[0].value_counts().sort_index(ascending=[True]).tolist())
-         return f
+        print("Batch " + str(i))
+        countClasses("Gas_sensor_dataset/dataset/batch" + str(i) + ".dat")
 
-    print("Batch " + str(i))
-    countClasses("Gas_sensor_dataset/dataset/batch" + str(i) + ".dat")
+def combine_data(path):
+    filenames = os.listdir(path)
+    with open('./combined_batches.dat','wb') as wfd:
+        for f in filenames:
+            with open(path + f,'rb') as fd:
+                shutil.copyfileobj(fd, wfd)
+
+combine_data("parsed_gas/")
+countClasses('./combined_batches.dat')
