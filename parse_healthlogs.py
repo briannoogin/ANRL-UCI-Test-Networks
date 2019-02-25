18f6698a62f2076429df6bde81cc8d30a95bcc73
Alan Liao <alanliao7@yahoo.com>
2019-02-24 20:06:28 -0600

    parse Gas sensor dataset

--- parse_healthlogs.py
+++ parse_healthlogs.py
@@ -1,8 +1,12 @@
 import sys
 
+#ARGS:
+#1: INPUT FILE
+#2: FORMATTED OUTPUT FILE
+
 with open(sys.argv[2], mode="w") as out:
     with open(sys.argv[1]) as log:
         for line in log:
             log_samples = line.split()
-            out.write("".join(num.ljust(12) for num in log_samples))
+            out.write("".join(num.ljust(8) for num in log_samples))
             out.write("\n");
