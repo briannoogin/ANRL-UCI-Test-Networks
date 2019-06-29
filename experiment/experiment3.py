b6a0d1b50e7d7109e38b742c9050d334dc164ee9
briannoogin <brian.qh.nguyen@gmail.com>
2019-06-29 03:25:55 -0500

    added transfer learning to mobilenet

--- experiment/experiment3.py
+++ experiment/experiment3.py
@@ -2,6 +2,8 @@ import keras
 from keras.datasets import cifar10
 from keras.applications.mobilenet import MobileNet
 from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, BatchNormalization, Input
+from keras.models import Model
 import math
 # cnn experiment 
 if __name__ == "__main__":
@@ -11,17 +13,30 @@ if __name__ == "__main__":
     x_train = x_train / 255
     x_test = x_test / 255
     datagen = ImageDataGenerator(
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
+    rotation_range=30,
+    width_shift_range=0.3,
+    height_shift_range=0.3,
     horizontal_flip=True,
     )
-    model = MobileNet(weights = None,classes=10,input_shape = (32,32,3),dropout = .5)
- 
+    #model = MobileNet(weights = None,classes=10,input_shape = (32,32,3),dropout = .25)
+    # transfer learning 
+    input_tensor = Input(shape=(32, 32, 3))
+    original_model = MobileNet(weights = 'imagenet',input_tensor = input_tensor, include_top = False, pooling = 'avg')
+    # freeze first 25 layers
+    for layer in original_model.layers[:25]:
+        layer.trainable=False
+    new_model = Dense(100, activation = 'relu')(original_model.output)
+    new_model = BatchNormalization()(new_model)
+    new_model = Dense(100, activation = 'relu')(new_model)
+    new_model = BatchNormalization()(new_model)
+    new_model = Dense(100, activation = 'relu')(new_model)
+    new_model = BatchNormalization()(new_model)
+    output = Dense(10, activation = 'softmax')(new_model)
+    model= Model(inputs=original_model.input,outputs=output)
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    #model.fit(x_train,y_train, batch_size=128,verbose=True,shuffle = True, epochs = 25)    
+ 
     num_samples = len(x_train)
     batch_size = 128
     steps_per_epoch = math.ceil(num_samples / batch_size)
-    model.fit_generator(datagen.flow(x_train,y_train,batch_size = batch_size),epochs = 50,validation_data = (x_test,y_test), steps_per_epoch = steps_per_epoch)
+    model.fit_generator(datagen.flow(x_train,y_train,batch_size = batch_size),epochs = 75,validation_data = (x_test,y_test), steps_per_epoch = steps_per_epoch, verbose = 1)
     print(model.evaluate(x_test,y_test))
